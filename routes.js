7179ba54f730781faccd92560842db48b97738a0
KhengVatanautdom <156300148+KhengVatanautdom@users.noreply.github.com>
2025-03-02 16:25:56 +0700

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { searchProducts } = require('./updateService');
+const { updateProducts } = require('./updateService');
 
 
 // Route to update a product
